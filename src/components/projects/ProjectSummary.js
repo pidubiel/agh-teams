baee6df379a98e1456c3ef50d8883341f0825c47
Piotr Dubiel <piotrdubiel@icloud.com>
2018-10-24 10:43:48 +0200

    Added SignUp, SignIn and Project Components

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+  return (
+    <div className="card grey lighten-5 z-depth-2 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>Posted by the Admin</p>
+        <p className="grey-text">3rd September, 5am</p>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummary
