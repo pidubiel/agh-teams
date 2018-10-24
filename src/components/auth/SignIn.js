22356c55daa4fdcb76e3ee31caccd920b8b05711
Piotr Dubiel <piotrdubiel@icloud.com>
2018-10-24 14:50:29 +0200

    Redux setup, CSS changes

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -18,20 +18,23 @@ class SignIn extends Component {
   render() {
     return (
       <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Sign In</h5>
-          <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange}/>
-          </div>
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange}/>
-          </div>
-          <div className="input-field">
-            <button className="btn red lighten-1 waves-effect waves-light">Login</button>
-          </div>
-        </form>
+        <div className="row">
+          <form onSubmit={this.handleSubmit} className="white col l6 s12 offset-l3">
+            <h5 className="grey-text text-darken-3">Sign In</h5>
+            <div className="input-field">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" onChange={this.handleChange}/>
+            </div>
+            <div className="input-field">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" onChange={this.handleChange}/>
+            </div>
+            <div className="input-field">
+              <button className="btn red lighten-1 waves-effect waves-light">Login</button>
+            </div>
+          </form>
+        </div>
+
       </div>
     )
   }
